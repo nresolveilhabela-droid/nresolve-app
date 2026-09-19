204976689382eb4c8003cec488f39e14dcbc4462
nresolveilhabela-droid <nresolve.ilhabela@gmail.com>
2026-09-19 13:09:08 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE = 'nresolve-v1-0-62';
+const APP_SHELL = ['./', './manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(APP_SHELL))
+      .catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  const url = new URL(event.request.url);
+
+  if (url.hostname.includes('supabase.co')) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE)
+            .then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() =>
+          caches.match(event.request)
+            .then(response => response || caches.match('./'))
+        )
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE)
+          .then(cache => cache.put(event.request, copy));
+        return response;
+      })
+    )
+  );
+});
