d48a2f4c5f4ba37d28364ddf34c18f88eeb09151
nresolveilhabela-droid <nresolve.ilhabela@gmail.com>
2026-09-20 12:26:23 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='nresolve-v1-0-86';
+const CACHE='nresolve-v1-0-87';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{
   event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
